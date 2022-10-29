7dcc6ae3b5ee07255201b1456601b8b797925a59
theo_cha <cha_theo@hotmail.com>
2022-10-29 20:19:01 +0300

    charts test

--- services/stats_service.py
+++ services/stats_service.py
@@ -8,25 +8,39 @@ from flask import Blueprint, jsonify, request
 
 # find values for charts
 
-a = "test text"
+a =1
 b = [10,20,30,40]
+df = pd.read_csv(os.getcwd() + '/repo/listings_preprocessed.csv', encoding="utf8")
+
+max_price_per_room_type = []
 
 
+#max price per room type
+for x in df.groupby('room_type')['price'].max():
+    max_price_per_room_type.append(x)
+
+print(df.groupby('room_type')['price'].max().keys()[0])
 #test chart to fill with values
 charts = {
-    "barChart": [{"pv": a,
-                 "uv": b[0],
-                  "amt": 2400,
-                  "name": "Page AAAAavvvvvvaAA"},
-                 {"pv": b[1],
-                 "uv": 4000,
-                  "amt": 2400,
-                  "name": "Page AAAAaaAA"}],
-
-    "pieChart": [{"pv": 2400,
-                 "uv": 4000,
-                  "amt": 2400,
-                  "name": "Page A"}],
+    "barChart": [{"Max price": df.groupby('room_type')['price'].max()[0],
+                  "name": df.groupby('room_type')['price'].max().keys()[0],
+                  "Mean price": round(df.groupby('room_type')['price'].mean()[0],2)},
+                  {"Max price": df.groupby('room_type')['price'].max()[1],
+                  "name": df.groupby('room_type')['price'].max().keys()[1],
+                  "Mean price": round(df.groupby('room_type')['price'].mean()[1],2)},
+                  {"Max price": df.groupby('room_type')['price'].max()[2],
+                  "name": df.groupby('room_type')['price'].max().keys()[2],
+                  "Mean price": round(df.groupby('room_type')['price'].mean()[2],2)},
+                  {"Max price": df.groupby('room_type')['price'].max()[3],
+                  "name": df.groupby('room_type')['price'].max().keys()[3],
+                  "Mean price": round(df.groupby('room_type')['price'].mean()[3],2)},
+                  ],
+    "pieChart": [{"name": "Page A",
+                "value": 1000},
+                {"name": "Page A",
+                "value": 1000},
+                {"name": "Page A",
+                "value": 1000}],
 
     "areaChart": [{"pv": 2400,
                   "uv": 4000,
