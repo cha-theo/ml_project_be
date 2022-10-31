3ec8272a8a818a4b3a72ecd3efc18ee24a69f69e
theo_cha <cha_theo@hotmail.com>
2022-10-31 03:12:40 +0200

    minor change with extra print

--- services/stats_service.py
+++ services/stats_service.py
@@ -3,6 +3,9 @@ import os
 import pandas as pd
 import logging 
 from flask import Blueprint, jsonify, request
+import pickle
+
+loaded_model = pickle.load(open('repo/finalized_model.sav', 'rb'))
 
 
 
@@ -18,7 +21,7 @@ max_price_per_room_type = []
 for x in df.groupby('room_type')['price'].max():
     max_price_per_room_type.append(x)
 
-print(df.groupby('room_type')['price'].max().keys()[0])
+
 #test chart to fill with values
 
 charts = {
@@ -73,7 +76,7 @@ def max_host_id():
     return charts
 
 
-#we get the data from the form as dictionary and we return the best price based on our model
+# we get the data from the form as dictionary and we return the best price based on our model
 # Form dictionary values are(we will change these):
 # textField 
 # textField2 
@@ -91,9 +94,20 @@ def test_second_function(a,b,c):
     return z
 
 
+#test inout dictionary values
+dict = {'accommodates':[2],'beds':[2],'smoke_alarm':[0],'oven':[1],'patio_balcony':[1],'fire_extinguisher':[0],'shower_gel':[1]}
+data2 = pd.DataFrame.from_dict(dict)
+az = loaded_model.predict(data2)
+print(az)
+
+
 def test_function_with_data(data):
     #here we can assign the prices to the model and get the price
     a = test_second_function(data["textField"], data["textField2"], data["switchField"]) 
-    
-    return ("The best price for your home is: {}".format(a))
+    data["textField"] = [int(data["textField"])]
+    return ("The best price for your home is: {}".format(az))
+
+# input fields for model function
+# accommodates, beds, smoke_alarm, oven, patio_balcony, fire_extinguisher, shower_gel
+
 
